1cb56ad30de40baf46bf8e9c1d742db412b1b140
ofek512 <ofekikosh123@gmail.com>
2025-06-12 14:45:17 +0300

    Implement Song and genre classes

--- Genre.h
+++ Genre.h
@@ -5,8 +5,79 @@
 #ifndef DS_WET2_GENRE_H
 #define DS_WET2_GENRE_H
 
+#include "Song.h"
+#include <memory>
+
+using namespace std;
+
+// the genre clase will hold a pointer to the root of the union find, which is a song.
+// the genre class has genre id, size.
+
+/// forward declaration of class song
+
+class Song;
 
 class Genre {
+
+private:
+    shared_ptr<Song> root; // pointer to the root of the union find
+    int genre_id;
+    int size;
+
+public:
+    // Constructor
+    Genre(int id) : genre_id(id), size(0), root(nullptr) {}
+
+    // Destructor
+    ~Genre() = default;
+
+    // Getters
+    int getGenreId() const {
+        return genre_id;
+    }
+
+    int getSize() const {
+        return size;
+    }
+
+    shared_ptr<Song> getRoot() const {
+        return root;
+    }
+
+    // Increment and decrement size
+    void incrementSize() {
+        size++;
+    }
+
+    void decrementSize() {
+        if (size > 0) {
+            size--;
+        }
+    }
+
+    // Setters
+    void setRoot(shared_ptr<Song> new_root) {
+        root = new_root;
+    }
+
+    // cast to int
+    operator int() const {
+        return genre_id;
+    }
+
+    // Comparison operators
+    bool operator==(const Genre &other) const {
+        return genre_id == other.genre_id;
+    }
+
+    bool operator!=(const Genre &other) const {
+        return !(*this == other);
+    }
+
+    bool operator<(const Genre &other) const {
+        return genre_id < other.genre_id;
+    }
+
 
 };
 
--- Song.h
+++ Song.h
@@ -5,8 +5,81 @@
 #ifndef DS_WET2_SONG_H
 #define DS_WET2_SONG_H
 
+#include <memory>
+#include "Genre.h"
 
+using namespace std;
 
+class Song {
+private:
+    shared_ptr<Genre> genre;
+    shared_ptr<Song> father; // pointer to the father song in the union find
+    int song_id;
+    int genreChanges;
+
+public:
+    // constructor
+    Song(int id) : song_id(id), genreChanges(0), father(nullptr),
+                   genre(nullptr) {}
+
+    // destructor
+    ~Song() = default;
+
+    // getters
+    int getSongId() const {
+        return song_id;
+    }
+
+    int getGenreChanges() const {
+        return genreChanges;
+    }
+
+    shared_ptr<Genre> getGenre() const {
+        return genre;
+    }
+
+    shared_ptr<Song> getFather() const {
+        return father;
+    }
+
+    // setters
+    void setGenre(shared_ptr<Genre> new_genre) {
+        genre = new_genre;
+    }
+
+    void setFather(shared_ptr<Song> new_father) {
+        father = new_father;
+    }
+
+    void incrementGenreChanges() {
+        genreChanges++;
+    }
+
+    void decrementGenreChanges() {
+        if (genreChanges > 0) {
+            genreChanges--;
+        }
+    }
+
+    // cast to int
+    operator int() const {
+        return song_id;
+    }
+
+    // comparison operator
+    bool operator==(const Song &other) const {
+        return song_id == other.song_id;
+    }
+
+    bool operator!=(const Song &other) const {
+        return !(*this == other);
+    }
+
+    bool operator<(const Song &other) const {
+        return song_id < other.song_id;
+    }
+
+};
 
 
 #endif //DS_WET2_SONG_H

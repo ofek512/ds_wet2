d514b018f94e75bba42aed7336865c62958ec147
Ofek Yankis <95701220+ofek512@users.noreply.github.com>
2025-06-15 20:19:15 +0300

    Update Hash.h

--- Hash.h
+++ Hash.h
@@ -1,140 +1,105 @@
-//
-// Created by nival on 1/20/2025.
-//
+#ifndef HASHMAP_H
+#define HASHMAP_H
 
-#ifndef HASHTABLE_H
-#define HASHTABLE_H
 #include <memory>
+#include "wet2util.h"
 #include "LinkedList.h"
 
-#endif //HASHTABLE_H
-
-template <class T>
-class HashTable {
+template<typename Item>
+class HashMap {
 public:
-    HashTable(bool isRecordTable);
-    void resize();
-    bool insert(int id, std::shared_ptr<T> val);
-    bool remove(int id);
-    bool removeByRecord(int id, int record);
-    std::shared_ptr<T> search(int id);
-    std::shared_ptr<T> searchByRecord(int id, int record);
-    int countInstances(int num);
+    explicit HashMap();
 
+    bool add(int key, std::shared_ptr<Item> value);
+    bool erase(int key);
+    std::shared_ptr<Item> find(int key) const;
+    int count(int key) const;
 
+    int getCapacity() const { return capacity_; }
+
+    // now returns the raw pointer to the list, not the unique_ptr wrapper
+    LinkedList<Item>* getBucket(int index) const {
+        if (index >= 0 && index < capacity_) {
+            return buckets_[index].get();
+        }
+        return nullptr;
+    }
 
 private:
-    std::unique_ptr<std::unique_ptr<LinkedList<T>>[]> values;
-    int countValues;
-    int length;
-    int computeHash(const int key, int size) const { return  (key % size + size) % size;}
-    bool recordTable = false;
+    void expand();
+    int hashIndex(int key, int bucket_count) const {
+        return ((key % bucket_count) + bucket_count) % bucket_count;
+    }
+
+    int capacity_;
+    int size_;
+    std::unique_ptr<std::unique_ptr<LinkedList<Item>>[]> buckets_;
 };
 
-
-
-template<class T>
-HashTable<T>::HashTable(bool isRecordTable) {
-    recordTable = isRecordTable;
-    countValues = 0;
-    length = 16;
-    values = std::unique_ptr<std::unique_ptr<LinkedList<T>>[]>(new std::unique_ptr<LinkedList<T>>[length]);
-    for (int i = 0; i < length; i++) {
-        values[i] = std::unique_ptr<LinkedList<T>>(new LinkedList<T>);
+template<typename Item>
+HashMap<Item>::HashMap()
+        : capacity_(16),
+          size_(0),
+          buckets_(new std::unique_ptr<LinkedList<Item>>[capacity_])
+{
+    for (int i = 0; i < capacity_; ++i) {
+        buckets_[i] = std::make_unique<LinkedList<Item>>();
     }
 }
 
-
-template<class T>
-void HashTable<T>::resize() {
-    int tempLength = length*2;
-    std::unique_ptr<std::unique_ptr<LinkedList<T>>[]> newValues = std::unique_ptr<std::unique_ptr<LinkedList<T>>[]>(new std::unique_ptr<LinkedList<T>>[tempLength]);
-    for (int i = 0; i < tempLength; i++) {
-        newValues[i] = std::unique_ptr<LinkedList<T>>(new LinkedList<T>);
+template<typename Item>
+void HashMap<Item>::expand() {
+    int new_capacity = capacity_ * 2;
+    auto new_buckets = std::unique_ptr<std::unique_ptr<LinkedList<Item>>[]>(
+            new std::unique_ptr<LinkedList<Item>>[new_capacity]
+    );
+    for (int i = 0; i < new_capacity; ++i) {
+        new_buckets[i] = std::make_unique<LinkedList<Item>>();
     }
-    for (int i = 0; i < length; i++) {
-        int listSize = values[i]->getSize();
-        for (int j = 0; j < listSize; j++) {
-            std::shared_ptr<T> tempValue = values[i]->removeFirst();
-            int tempID = computeHash(tempValue->getID(), tempLength);
-            newValues[tempID]->insert(tempValue, tempValue->getID());
+    for (int i = 0; i < capacity_; ++i) {
+        int list_size = buckets_[i]->getSize();
+        for (int j = 0; j < list_size; ++j) {
+            auto node_val = buckets_[i]->removeFirst();
+            int idx = hashIndex(node_val->getID(), new_capacity);
+            new_buckets[idx]->insert(node_val, node_val->getID());
         }
     }
-    length = tempLength;
-    values = std::move(newValues);
+    capacity_ = new_capacity;
+    buckets_ = std::move(new_buckets);
 }
 
-template<class T>
-bool HashTable<T>::insert(int id, std::shared_ptr<T> val) {
-    countValues++;
-    if (countValues > length*2) {
-        resize();
+template<typename Item>
+bool HashMap<Item>::add(int key, std::shared_ptr<Item> value) {
+    ++size_;
+    if (size_ > capacity_ * 2) {
+        expand();
     }
-
-    int hashValue = computeHash(id, length);
-    values[hashValue]->insert(val, id);
+    int idx = hashIndex(key, capacity_);
+    buckets_[idx]->insert(value, key);
     return true;
 }
 
-template<class T> //TODO can maybe remove the check from here because they are already made in plains
-bool HashTable<T>::remove(int id) {
-    int hashValue = computeHash(id, length);
-    if (values[hashValue]->search(id)) {
-        values[hashValue]->remove(id);
-        countValues--;
+template<typename Item>
+bool HashMap<Item>::erase(int key) {
+    int idx = hashIndex(key, capacity_);
+    if (buckets_[idx]->search(key)) {
+        buckets_[idx]->remove(key);
+        --size_;
         return true;
     }
     return false;
 }
 
-template<class T>
-bool HashTable<T>::removeByRecord(int id, int record){
-
-    int hashValue = computeHash(record, length);
-    std::shared_ptr<Node<T>> currentNode = values[hashValue]->getStart(),
-            previousNode = currentNode;
-    while(currentNode != nullptr){
-        if(currentNode->getData()->getID() == id){
-            if (currentNode->getData()->getID() == values[hashValue]->getStart()->getData()->getID()){
-                values[hashValue]->setStart(values[hashValue]->getStart()->getNext());
-            } else{
-                previousNode->setNext(currentNode->getNext());
-            }
-            values[hashValue]->decreaseSize();
-            countValues--;
-            return true;
-        }
-        previousNode = currentNode;
-        currentNode = currentNode->getNext();
-    }
-    return false;
+template<typename Item>
+std::shared_ptr<Item> HashMap<Item>::find(int key) const {
+    int idx = hashIndex(key, capacity_);
+    return buckets_[idx]->getValue(key);
 }
 
-
-template<class T>
-std::shared_ptr<T> HashTable<T>::search(int id) {
-    int hashValue = computeHash(id, length);
-    return values[hashValue]->getValue(id); //This function returns nullptr if doesn't exist
+template<typename Item>
+int HashMap<Item>::count(int key) const {
+    int idx = hashIndex(key, capacity_);
+    return buckets_[idx]->countInstances(key);
 }
 
-
-//TODO needs to be checked for new behavior
-template<class T>
-std::shared_ptr<T> HashTable<T>::searchByRecord(int id, int record){
-    int hashValue = computeHash(record, length);
-    auto currentNode = values[hashValue]->getStart();
-    while(currentNode != nullptr){
-        if(currentNode->getData()->getID() == id){
-            return currentNode->getData();
-        }
-        currentNode = currentNode->getNext();
-    }
-    return nullptr;
-}
-
-
-template<class T>
-int HashTable<T>::countInstances(int num) {
-    int hashValue = computeHash(num, length);
-    return values[hashValue]->countInstances(num);
-}
+#endif // HASHMAP_H

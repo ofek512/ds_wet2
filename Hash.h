9d132c93bb43ed94f98b77a4ddc2a8de06d0b94d
Kingslayer2001 <shcherbina.daniel@gmail.com>
2025-06-26 17:02:48 +0300

    ~hash tweak

--- Hash.h
+++ Hash.h
@@ -87,6 +87,14 @@ public:
     }
 
     ~Hash() {
+        for(int i = 0; i < max_size; ++i) {
+            shared_ptr<Node<T>> currentNode = table[i];
+            while (currentNode) {
+                shared_ptr<Node<T>> prevNode = currentNode;
+                currentNode = currentNode->next;
+                delete prevNode;
+            }
+        }
         delete[] table;
         table = nullptr;
     }
